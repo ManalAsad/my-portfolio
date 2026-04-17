ea6c5d3de804459cd0400355d0d1bac56382a193
ManalAsad <manalasad18@gmail.com>
2026-04-16 23:42:30 -0700

    Initial portfolio commit

--- src/Main.jsx
+++ src/Main.jsx
@@ -7,7 +7,7 @@ import './Blog.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/my-portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
